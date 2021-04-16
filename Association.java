ab425aa97ca0360610b2b1112e06c0ed8cc8ee8d
Juan Andrés Galicia <69065800+JuanGalicia20@users.noreply.github.com>
2021-04-16 17:52:42 -0600

    Se añadio clase Association y Objetos Palabra

---
+++ Association.java
@@ -0,0 +1,18 @@
+public class Association<K,T> {
+    public T _value;
+	public K _key;
+	public Association<K, T> hijoIzquierdo;
+	public Association<K, T> hijoDerecho;
+	
+	public Association() {
+		hijoIzquierdo = null;
+		hijoDerecho = null;
+	}
+	
+	public Association(K key, T value) {
+		_key = key;
+		_value = value;
+		hijoIzquierdo = null;
+		hijoDerecho = null;
+	}
+}
---
+++ Palabra.java
@@ -0,0 +1,38 @@
+public class Palabra {
+    private String ingles;
+    private String espanol;
+    private String frances;
+
+
+    
+
+
+    public String getIngles() {
+        return ingles;
+    }
+    public void setIngles(String ingles) {
+        this.ingles = ingles;
+    }
+    public String getEspanol() {
+        return espanol;
+    }
+    public void setEspanol(String espanol) {
+        this.espanol = espanol;
+    }
+    public String getFrances() {
+        return frances;
+    }
+    public void setFrances(String frances) {
+        this.frances = frances;
+    }
+    public Palabra(String ingles, String espanol, String frances) {
+        this.ingles = ingles;
+        this.espanol = espanol;
+        this.frances = frances;
+    }
+    public String LLave() {
+        return null;
+    }
+
+    
+}
